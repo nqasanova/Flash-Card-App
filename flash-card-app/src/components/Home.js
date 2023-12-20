b0f7c2de1d263bd96c92a90a27fa81771daa61af
Natavan Hasanova <natavanhesenova4@gmail.com>
2023-12-20 16:39:28 +0400

    Implement a navbar component.

---
+++ flash-card-app/src/components/Home.js
@@ -0,0 +1,16 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Flash Card App!</h1>
+      {/* Display information about projects with external links */}
+      <p>Project 1: <a href="project1-link">Project 1 Title</a></p>
+      <p>Project 2: <a href="project2-link">Project 2 Title</a></p>
+      {/* Add more projects as needed */}
+    </div>
+  );
+};
+
+export default Home;
---
+++ flash-card-app/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; 
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/flash-card" className="nav-link">Flash Card</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact" className="nav-link">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
