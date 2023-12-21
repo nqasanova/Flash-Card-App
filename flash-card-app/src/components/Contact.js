bb7a6c97cedfb2870758f6ba84ae0d5a4b37e346
Natavan Hasanova <natavanhesenova4@gmail.com>
2023-12-21 22:26:59 +0400

    Add comments to contact component as explanations.

--- flash-card-app/src/components/Contact.js
+++ flash-card-app/src/components/Contact.js
@@ -1,33 +1,40 @@
-// Contact.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import './Contact.css';
 
 const Contact = () => {
+  // State to hold the contact form information
   const [contactInfo, setContactInfo] = useState({
     subject: '',
     email: '',
     content: '',
   });
 
+  // Function to update the state when input fields change
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setContactInfo({ ...contactInfo, [name]: value });
   };
 
+  // Function to handle form submission
   const handleSubmit = async () => {
     try {
       console.log('Axios Config:', axios.defaults);
+
+      // Make a POST request to the messages endpoint with the contact information
       await axios.post('http://localhost:3000/messages', contactInfo);
+
+      // Clear the form after successful submission
       setContactInfo({ subject: '', email: '', content: '' });
+
       // Optionally, show a success message or redirect the user
     } catch (error) {
+      // Log an error message if there's an issue with the form submission
       console.error('Error submitting contact form:', error);
     }
   };
   
-  
-
+  // JSX rendering of the Contact component
   return (
     <div className="contact-container">
       <h1>Contact Us</h1>
