604e8f7d502438ed4ed89eeaa71b1d74899c8467
Natavan Hasanova <natavanhesenova4@gmail.com>
2023-12-21 22:30:54 +0400

    Add comments to app js as explanations.

--- flash-card-app/src/App.js
+++ flash-card-app/src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -7,13 +6,21 @@ import FlashCardsPage from './components/FlashCardsPage';
 import Contact from './components/Contact'; 
 import './App.css';
 
+// Functional component representing the main App
 const App = () => {
   return (
+    // Wrapping the entire application with the Router
     <Router>
+      {/* Rendering the Navbar component */}
       <Navbar />
       <Routes>
+        {/* Defining routes for different pages using Routes */}
         <Route path="/" element={<Home />} />
+
+        {/* Route for the Flash Cards page */}
         <Route path="/flash-cards" element={<FlashCardsPage />} />
+
+        {/* Route for the Contact page */}
         <Route path="/contact" element={<Contact />} /> 
       </Routes>
     </Router>
