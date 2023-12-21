c49b12c3f04c99e8378f27f80a6a2431bce38630
Natavan Hasanova <natavanhesenova4@gmail.com>
2023-12-21 22:14:03 +0400

    Add the feature to send the message to be stored in a JSON file.

--- flash-card-app/src/components/Contact.js
+++ flash-card-app/src/components/Contact.js
@@ -1,7 +1,7 @@
 // Contact.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Contact.css'; // Import the external CSS file
+import './Contact.css';
 
 const Contact = () => {
   const [contactInfo, setContactInfo] = useState({
@@ -14,25 +14,29 @@ const Contact = () => {
     const { name, value } = e.target;
     setContactInfo({ ...contactInfo, [name]: value });
   };
-
+  
   const handleSubmit = async () => {
     try {
+      console.log('Axios Config:', axios.defaults);
       await axios.post('http://localhost:3000/messages', contactInfo);
-      // Optionally, you can redirect the user
+      setContactInfo({ subject: '', email: '', content: '' });
+      // Optionally, show a success message or redirect the user
     } catch (error) {
       console.error('Error submitting contact form:', error);
     }
   };
+  
+  
 
   return (
     <div className="contact-container">
       <h1>Contact Us</h1>
       <div className="contact-form">
         <label>Subject:</label>
-        <input type="text" name="subject" class="subject" value={contactInfo.subject} onChange={handleInputChange} />
+        <input type="text" name="subject" className="subject" value={contactInfo.subject} onChange={handleInputChange} />
 
         <label>Email:</label>
-        <input type="text" name="email" class="email" value={contactInfo.email} onChange={handleInputChange} />
+        <input type="text" name="email" className="email" value={contactInfo.email} onChange={handleInputChange} />
 
         <label>Content:</label>
         <textarea name="content" value={contactInfo.content} onChange={handleInputChange} />
