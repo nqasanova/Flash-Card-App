5f8d875d40e5aa42c2620487d5a4dde0585cfa43
Natavan Hasanova <natavanhesenova4@gmail.com>
2024-01-04 10:14:53 +0400

    Update the router link in home page.

--- flash-card-app/src/App.js
+++ flash-card-app/src/App.js
@@ -1,9 +1,10 @@
+// Import necessary modules and components
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import FlashCardsPage from './components/FlashCardsPage';
-import Contact from './components/Contact'; 
+import Contact from './components/Contact';
 import './App.css';
 
 // Functional component representing the main App
@@ -14,14 +15,14 @@ const App = () => {
       {/* Rendering the Navbar component */}
       <Navbar />
       <Routes>
-        {/* Defining routes for different pages using Routes */}
-        <Route path="/" element={<Home />} />
+        {/* Set the default route to the Home component */}
+        <Route path="/Flash-Card-App" element={<Home />} />
 
         {/* Route for the Flash Cards page */}
         <Route path="/flash-cards" element={<FlashCardsPage />} />
 
         {/* Route for the Contact page */}
-        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
